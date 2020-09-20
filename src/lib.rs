rustls/rcgen

commit 127b392eb2651c88c716e6bccff9e9f88f21a41b

Add KeyPair::from_der
This is nice as the TryFrom implementation isn't immediately obvious.

src/lib.rs
@@ -1104,6 +1104,12 @@ pub struct KeyPair {
 }
 
 impl KeyPair {
+	/// Parses the key pair from the DER format
+	///
+	/// Equivalent to using the `TryFrom` implementation.
+	pub fn from_der(der :&[u8]) -> Result<Self, RcgenError> {
+		Ok(der.try_into()?)
+	}
 	/// Parses the key pair from the ASCII PEM format
 	///
 	/// *This constructor is only available if rcgen is built with the "pem" feature*

commit 5bf334444cdb6a1814cb14b695035a945610d2b2

Always import TryInto
Fixes CI failure when pem feature was turned off.
The failure was introduced by 127b392eb2651c88c716e6bccff9e9f88f21a41b

src/lib.rs
@@ -38,7 +38,6 @@ use yasna::Tag;
 use yasna::models::ObjectIdentifier;
 #[cfg(feature = "pem")]
 use pem::Pem;
-#[cfg(feature = "pem")]
 use std::convert::TryInto;
 use ring::digest;
 use ring::signature::{EcdsaKeyPair, Ed25519KeyPair, RsaKeyPair};