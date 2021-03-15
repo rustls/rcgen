c1d52d86919520bb3fe215943bb71a45ff5b381a
est31 <MTest31@outlook.com>
2021-03-15 02:26:53 +0100

    RRmm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+#![allow(unused)]
+
+extern crate rcgen;
+
+use rcgen::{Certificate, CertificateParams,
+	DistinguishedName, DnType, SanType,
+	date_time_ymd};
+use std::fs;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 	panic!();
 }
