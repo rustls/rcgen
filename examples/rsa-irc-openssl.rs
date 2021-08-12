d4ce76f88081ea9b43230e818ff7b2f3e9ca5a6b
est31 <MTest31@outlook.com>
2021-08-12 05:07:09 +0200

    Change the rsa-irc example to use the rsa crate for keygen
    Move the old rsa-irc example to rsa-irc-openssl

--- examples/rsa-irc-openssl.rs
+++ examples/rsa-irc-openssl.rs
@@ -1,17 +1,12 @@
 #![allow(clippy::complexity, clippy::style, clippy::pedantic)]
 
-extern crate rsa;
-extern crate rand;
 extern crate rcgen;
-
-use rsa::RsaPrivateKey;
-use rsa::pkcs8::ToPrivateKey;
-use rand::rngs::OsRng;
+extern crate openssl;
 
 use rcgen::{Certificate, CertificateParams,
 	DistinguishedName, date_time_ymd};
 use std::fs;
-use std::convert::TryFrom;
+use std::convert::TryInto;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let mut params :CertificateParams = Default::default();
@@ -21,11 +16,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 	params.alg = &rcgen::PKCS_RSA_SHA256;
 
-	let mut rng = OsRng;
-    let bits = 2048;
-	let private_key = RsaPrivateKey::new(&mut rng, bits)?;
-	let private_key_der = private_key.to_pkcs8_der()?;
-	let key_pair = rcgen::KeyPair::try_from(private_key_der.as_ref()).unwrap();
+	let pkey :openssl::pkey::PKey<_> = openssl::rsa::Rsa::generate(2048)?.try_into()?;
+	let key_pair_pem = String::from_utf8(pkey.private_key_to_pem_pkcs8()?)?;
+	let key_pair = rcgen::KeyPair::from_pem(&key_pair_pem)?;
 	params.key_pair = Some(key_pair);
 
 	let cert = Certificate::from_params(params)?;
