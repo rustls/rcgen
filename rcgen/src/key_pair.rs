a53aa2fb7599c16ef09e2d5a08dcdb53879e6829
Daniel McCarney <daniel@binaryparadox.net>
2023-10-27 17:27:09 -0400

    error: avoid leaking pem::PemError into api
    Having `From<pem::PemError>` defined on the public `Error`
    type means that the `pem` type leaks into rcgen's public API,
    complicating semver incompatible updates.
    
    This commit updates the sites that previously used this trait to
    use the crate internal `ExternalError` extension trait to map the
    `PemError` err to the generic rcgen `Error::PemError` err.
    
    Additionally, the `rcgen::Error::PemError` variant is changed to hold
    a `String` with the `pem::PemError` error string instead of the type
    itself. This allows the `From` impl on `Error` to be removed, fixing the
    type leak.

--- rcgen/src/key_pair.rs
+++ rcgen/src/key_pair.rs
@@ -68,7 +68,7 @@ impl KeyPair {
 	/// Parses the key pair from the ASCII PEM format
 	#[cfg(feature = "pem")]
 	pub fn from_pem(pem_str: &str) -> Result<Self, Error> {
-		let private_key = pem::parse(pem_str)?;
+		let private_key = pem::parse(pem_str)._err()?;
 		let private_key_der: &[_] = private_key.contents();
 		Ok(private_key_der.try_into()?)
 	}
@@ -91,7 +91,7 @@ impl KeyPair {
 		pem_str: &str,
 		alg: &'static SignatureAlgorithm,
 	) -> Result<Self, Error> {
-		let private_key = pem::parse(pem_str)?;
+		let private_key = pem::parse(pem_str)._err()?;
 		let private_key_der: &[_] = private_key.contents();
 		Ok(Self::from_der_and_sign_algo(private_key_der, alg)?)
 	}
@@ -380,6 +380,12 @@ impl<T> ExternalError<T> for Result<T, ring::error::KeyRejected> {
 impl<T> ExternalError<T> for Result<T, ring::error::Unspecified> {
 	fn _err(self) -> Result<T, Error> {
 		self.map_err(|_| Error::RingUnspecified)
+	}
+}
+
+impl<T> ExternalError<T> for Result<T, pem::PemError> {
+	fn _err(self) -> Result<T, Error> {
+		self.map_err(|e| Error::PemError(e.to_string()))
 	}
 }
 
