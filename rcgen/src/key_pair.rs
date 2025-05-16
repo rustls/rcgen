f8582f243899a7eff1a6524b42da60faaec73f15
Nikita Frolov <nf@mkmks.org>
2025-05-16 21:58:51 +0200

    added ECDSA secp256k1 signing

--- rcgen/src/key_pair.rs
+++ rcgen/src/key_pair.rs
@@ -282,6 +282,12 @@ impl KeyPair {
 					&serialized_der,
 					rng,
 				)?)
+			} else if alg == &PKCS_ECDSA_P256K1_SHA256 {
+				KeyPairKind::Ec(ecdsa_from_pkcs8(
+					&signature::ECDSA_P256K1_SHA256_ASN1_SIGNING,
+					&serialized_der,
+					rng,
+				)?)
 			} else {
 				panic!("Unknown SignatureAlgorithm specified!");
 			}
@@ -796,6 +802,8 @@ mod test {
 		for alg in [
 			&PKCS_ED25519,
 			&PKCS_ECDSA_P256_SHA256,
+			#[cfg(feature = "aws_lc_rs")]
+			&PKCS_ECDSA_P256K1_SHA256,
 			&PKCS_ECDSA_P384_SHA384,
 			#[cfg(feature = "aws_lc_rs")]
 			&PKCS_ECDSA_P521_SHA512,
