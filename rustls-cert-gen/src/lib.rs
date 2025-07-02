001bd6a71892304f6edf439409955a3a6ffd4774
Dirkjan Ochtman <dirkjan@ochtman.nl>
2025-07-02 11:00:23 +0200

    Collect lint directives in one place

--- rustls-cert-gen/src/lib.rs
+++ rustls-cert-gen/src/lib.rs
@@ -1,10 +1,9 @@
-#![warn(missing_docs)]
 //! This library wraps [rcgen] to provide a simple API to generate TLS
 //! certificate-chains. Its primary intent is to ease development of
 //! applications that verify chains of trust. It can be used for
 //! whatever purpose you may need a TLS certificate-chain.
 
-#![warn(unreachable_pub)]
+#![warn(missing_docs, unreachable_pub)]
 
 mod cert;
 pub use cert::{Ca, CaBuilder, CertificateBuilder, EndEntity, EndEntityBuilder};
