6ad7872673bb2a621d2acf5767106b79125a06b3
Joe Birr-Pixton <jpixton@gmail.com>
2026-02-26 21:29:26 +0000

    Ensure `SignatureAlgorithm::iter()` works for ML-DSA

--- rcgen/src/sign_algo.rs
+++ rcgen/src/sign_algo.rs
@@ -78,6 +78,12 @@ impl SignatureAlgorithm {
 			#[cfg(feature = "aws_lc_rs")]
 			&ECDSA_P521_SHA512,
 			&ED25519,
+			#[cfg(feature = "aws_lc_rs")]
+			&ML_DSA_44,
+			#[cfg(feature = "aws_lc_rs")]
+			&ML_DSA_65,
+			#[cfg(feature = "aws_lc_rs")]
+			&ML_DSA_87,
 		];
 		ALGORITHMS.iter()
 	}
