848ff3a8d27fa2b7a0e2c6bdc3a2b7449d0e8925
Dirkjan Ochtman <dirkjan@ochtman.nl>
2026-09-22 12:04:18 +0200

    Rename SanType to GeneralName, matching RFC 5280 nomenclature

--- rustls-cert-gen/src/cert.rs
+++ rustls-cert-gen/src/cert.rs
@@ -9,7 +9,7 @@ use rcgen::DnValue::PrintableString;
 use rcgen::{
 	serialize_private_key_pem, BasicConstraints, Certificate, CertificateParams, CertifiedIssuer,
 	DistinguishedName, DnType, Error, ExtendedKeyUsagePurpose, IsCa, KeyPair, KeyUsagePurpose,
-	SanType, SignatureAlgorithm,
+	GeneralName, SignatureAlgorithm,
 };
 
 /// Builder to configure TLS [CertificateParams] to be finalized
@@ -161,10 +161,10 @@ impl EndEntityBuilder {
 			.push(DnType::CommonName, name);
 		self
 	}
-	/// `SanTypes` that will be recorded as
+	/// [`GeneralName`]s that will be recorded as
 	/// `subject_alt_names`. Multiple calls will append to previous
 	/// values.
-	pub fn subject_alternative_names(mut self, sans: Vec<SanType>) -> Self {
+	pub fn subject_alternative_names(mut self, sans: Vec<GeneralName>) -> Self {
 		self.params.subject_alt_names.extend(sans);
 		self
 	}
@@ -456,13 +456,13 @@ mod tests {
 			.build()
 			.unwrap();
 		let name = "unexpected.oomyoo.xyz";
-		let names = vec![SanType::DnsName(name.try_into().unwrap())];
+		let names = vec![GeneralName::DnsName(name.try_into().unwrap())];
 		let params = CertificateParams::default();
 		let cert = EndEntityBuilder::new(params, KeyPairAlgorithm::default())
 			.subject_alternative_names(names);
 		assert_eq!(
 			cert.params.subject_alt_names,
-			vec![rcgen::SanType::DnsName(name.try_into().unwrap())]
+			vec![rcgen::GeneralName::DnsName(name.try_into().unwrap())]
 		);
 	}
 	#[test]
